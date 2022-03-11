1c931b79c4ce7c1502b81a201e0ddfdc2a6cb9b8
denizcamalan <denizcamalann@gmail.com>
2022-03-12 02:29:13 +0300

    solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a,b) {
+  return a+b;
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  return a*b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1;
+	for (let i=1; i<=b; i++){
+    result = result *a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+  else
+	  for (let i=1; i<=a; i++){
+      return i++;
+    }
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reversed = [];
+    string = string.split("");
+        for (let i=0; i<string.lenth-1; i++){
+            reversed[i] = string[(string.lenth-1)-i];
+        }
+            reversed = reversed.toString;
+            string = string.toString;
+    if (reversed == string) return true;
+    else return false;
 };
 
 // Do not edit below this line
